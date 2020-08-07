994e08acd9771e726399245cf5d7c6cbc4b52af4
atz3n <A_Aldemir@hotmail.de>
2020-08-07 18:03:48 +0200

    updated resolver to bba

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,59 @@
-import { baaMethodHandler } from "@blobaa/baa-did-method-handler-ts";
+import { bbaMethodHandler, Error, ErrorCode } from "@blobaa/bba-did-method-handler-ts";
 import express from "express";
+import dotenv from "dotenv";
 
 
-const mainnetUrl = "https://ardor.jelurida.com";
-const testnetUrl = "https://testardor.jelurida.com";
-
-const PORT = 8080;
+dotenv.config();
+const mainnetUrl = process.env.MAINNET_ADDRESS || "";
+const testnetUrl = process.env.TESTNET_ADDRESS || "";
+const PORT = process.env.PORT || 8080;
 
 
 const app = express();
 app.get("/1.0/identifiers/:did", async (req, res) => {
     let url = "";
 
-
-    if (req.params.did.startsWith("did:baa:t:")) {
+    if (req.params.did.startsWith("did:bba:t:")) {
         url = testnetUrl;
     } else {
         url = mainnetUrl;
     }
-
+// did:bba:t:12a4ed8ec741c14e5adf8e560db261877da5c6a825e79e88de7a8470f52b87ef
 
     try {
-        const response = await baaMethodHandler.resolveDID(url, { did: req.params.did });
+        
+        const response = await bbaMethodHandler.resolveDID(url, { did: req.params.did });
+        res.setHeader('Content-Type', 'application/json');
         res.send(response.didDocument);
-    } catch (error) {
-        console.log(error);
-        res.status(404).send(error.description);
+
+    } catch (e) {
+
+        console.log(e);
+        const error = e as Error;
+        if (isInvalidInput(error)) {
+            res.status(400).send("invalid input!");
+        } else if (isResolutionError(error)) {
+            res.status(404).send("not found");
+        } else {
+            res.status(500).send("error!");
+        }
+
     }
 
 });
 
+const isInvalidInput = (error : Error): boolean => {
+    return error.code === ErrorCode.INVALID_DID_NETWORK_TYPE || error.code === ErrorCode.INVALID_DID_IDENTIFIER ||
+           error.code === ErrorCode.INVALID_DID_METHOD || error.code === ErrorCode.INVALID_DID_METHOD_SPECIFIC_IDENTIFIER
+}
+
+const isResolutionError = (error: Error): boolean => {
+    return error.code === ErrorCode.DID_RESOLUTION_ERROR || error.code === ErrorCode.DIDDOC_NOT_FOUND ||
+           error.code === ErrorCode.DID_NOT_FOUND
+}
+
+
 
 app.listen(PORT, () => {
-  console.log(`baa Resolver driver running on port ${ PORT }...`);
+  console.log(`baa resolver driver running on port ${ PORT }...`);
 });
